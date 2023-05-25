7987d9ff9ecd3d98eaac69593c9788f0770896f7
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-05-25 09:28:09 -0700

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,7 +6,6 @@
  * @separator: string to be printed between the strings
  * @n: number of strings passed to the function
  *
- * Return: void
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
