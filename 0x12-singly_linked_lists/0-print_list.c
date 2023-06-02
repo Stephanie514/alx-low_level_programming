3103b716df7b8c4d7ceed11a94525e8211db2237
Steffanie Chepkirui <chepkiruistephanie514@gmail.com>
2023-06-02 05:02:13 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,24 +3,25 @@
 
 /**
  * print_list - is the name of the function.
- * size_t - return type of the function.
  *
- * print_list - is the name of the function.
  * @h: is the parameter of the function.pointer to a constant list_t structure
  * Return: it returns the number of elements printed
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t num = 0;
+	const list_t *newPointer = h;
 
-	while (h)
+	while (newPointer)
 	{
-		if (!h->str)
+		if (!newPointer->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u]%s\n", h->len, h->str);
-		h = h->next;
+			printf("[%u] %s\n", newPointer->len, newPointer->str);
+		newPointer = newPointer->next;
 		num++;
 	}
 	return (num);
 }
+
