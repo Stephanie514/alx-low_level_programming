b55b9b12d90fcab9e22dbb963578e6535986b09c
Steffanie Chepkirui <chepkiruistephanie514@gmail.com>
2023-06-02 04:34:50 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,20 +11,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t num;
+	size_t num = 0;
 
-	num = 0;
-
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-		{
+		if (!h->str)
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u]%s\n", h->len, h->str);
-		}
 		h = h->next;
 		num++;
 	}
