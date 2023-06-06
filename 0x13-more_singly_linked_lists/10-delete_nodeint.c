280a1ae726f5f5682c17622c78dfb8369ce5cbd4
Steffanie Chepkirui <chepkiruistephanie514@gmail.com>
2023-06-06 07:13:15 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		pres = pres->next;
 		num++;
 
-		if (pres == NULL)
+		if (pres == NULL || pres->next == NULL)
 			return (-1);
 	} while (num < index);
 	prev->next = pres->next;
