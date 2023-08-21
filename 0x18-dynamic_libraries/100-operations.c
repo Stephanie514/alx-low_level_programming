beb065c26a0f2996a35cadc829ed937bbd6357db
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-21 12:16:47 +0000

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int sub(int i, int j)
+{
+return (i - j);
+}
+
+int add(int i, int j)
+{
+return (i + j);
+}
+
+int div(int i, int j)
+{
+return (i / j);
+}
+
+int mul(int i, int j)
+{
+return (i * j);
+}
+
+int mod(int i, int j)
+{
+return (i % j);
+}
