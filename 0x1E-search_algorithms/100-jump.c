a2c368bd6c67bacd782c28cbc67ac73562784684
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-01-17 14:05:56 -0800

    Corrected 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -6,7 +6,7 @@
  * @size: size of array
  * @value: value to be searched
  *
- * Return: index of value searched or -1
+ * Return: first index of value searched or -1 if not found
  */
 int jump_search(int *array, size_t size, int value)
 {
@@ -15,7 +15,8 @@ size_t currentIndex, jumpIndex, stepSize;
 if (array == NULL || size == 0)
 return (-1);
 
-for (currentIndex = jumpIndex = 0, stepSize = sqrt(size);
+stepSize = sqrt(size);
+for (currentIndex = 0, jumpIndex = stepSize;
 jumpIndex < size && array[jumpIndex] < value;
 currentIndex = jumpIndex, jumpIndex += stepSize)
 {
@@ -25,10 +26,12 @@ printf("Value checked array[%ld] = [%d]\n", jumpIndex, array[jumpIndex]);
 printf("Value found between indexes [%ld] and [%ld]\n",
 currentIndex, jumpIndex);
 
-jumpIndex = jumpIndex < size ? jumpIndex : size;
-for (; currentIndex < jumpIndex; ++currentIndex)
+jumpIndex = (jumpIndex < size) ? jumpIndex : size - 1;
+
+for (; currentIndex <= jumpIndex; ++currentIndex)
 {
 printf("Value checked array[%ld] = [%d]\n", currentIndex, array[currentIndex]);
+
 if (array[currentIndex] == value)
 {
 printf("Found %d at index: %ld\n", value, currentIndex);
