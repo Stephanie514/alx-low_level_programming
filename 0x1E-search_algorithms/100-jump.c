d6b6b0bf52389cffa50a558ba624c5b0640e0101
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-01-17 14:34:14 -0800

    Task 7 C

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,43 +1,35 @@
 #include "search_algos.h"
 
 /**
- * jump_search - searches for a value in a sorted array of integers
- * @array: array of integers
- * @size: size of array
- * @value: value to be searched
+ * jump_search - This searches for a value in sorted array of integers
+ * @array: An array of integers
+ * @size: The size of array
+ * @value: The value at be searched
  *
- * Return: first index of value searched or -1 if not found
+ * Return: The index of value searched or -1
  */
+
 int jump_search(int *array, size_t size, int value)
 {
-size_t currentIndex, jumpIndex, stepSize;
+size_t a, Indexjump, sizeStep;
 
 if (array == NULL || size == 0)
 return (-1);
 
-stepSize = sqrt(size);
-for (currentIndex = 0, jumpIndex = stepSize;
-jumpIndex < size && array[jumpIndex] < value;
-currentIndex = jumpIndex, jumpIndex += stepSize)
+sizeStep = sqrt(size);
+for (a = 0, Indexjump = 0; Indexjump < size && array[Indexjump] < value;)
 {
-printf("Value checked array[%ld] = [%d]\n", currentIndex, array[currentIndex]);
+printf("Value checked array[%ld] = [%d]\n", Indexjump, array[Indexjump]);
+a = Indexjump;
+Indexjump += sizeStep;
 }
 
-printf("Value found between indexes [%ld] and [%ld]\n",
-currentIndex, jumpIndex);
+printf("Value between indexes [%ld] and [%ld]\n", a, Indexjump);
 
-jumpIndex = (jumpIndex < size) ? jumpIndex : size - 1;
+Indexjump = (Indexjump < size - 1) ? Indexjump : size - 1;
+for (; a < Indexjump && array[a] < value; a++)
+printf("Value checked array[%ld] = [%d]\n", a, array[a]);
+printf("Value checked array[%ld] = [%d]\n", a, array[a]);
 
-for (; currentIndex <= jumpIndex; ++currentIndex)
-{
-printf("Value checked array[%ld] = [%d]\n", currentIndex, array[currentIndex]);
-
-if (array[currentIndex] == value)
-{
-printf("Found %d at index: %ld\n", value, currentIndex);
-return (currentIndex);
-}
-}
-
-return (-1);
+return (array[a] == value ? (int)a : -1);
 }
