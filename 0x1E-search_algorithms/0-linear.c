6e469009872ad6c7fc4f6b6f534a5e0d1fe554d4
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-01-17 10:25:44 -0800

    Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,12 +1,12 @@
 #include "search_algos.h"
 /**
-* linear_search - searches for a value in an array (linear search)
-* @array: array of integers
-* @size: size of array
-* @value: value that should be found
-*
-* Return: index of value searched or -1 if it doesn't exist
-*/
+ * linear_search - searches for a value in an array (linear search)
+ * @array: array of integers
+ * @size: size of array
+ * @value: value that should be found
+ *
+ * Return: index of value searched or -1 if it doesn't exist
+ */
 int linear_search(int *array, size_t size, int value)
 {
 size_t a;
@@ -17,10 +17,9 @@ for (a = 0; a < size; a++)
 printf("Value checked array[%lu] = [%d]\n", a, array[a]);
 if (value == array[a])
 {
-printf("Value %d found at index %lu\n", value, a);
+printf("Found %d at index %lu\n", value, a);
 return (a);
 }
 }
-printf("Value %d not present in array\n", value);
 return (-1);
 }
