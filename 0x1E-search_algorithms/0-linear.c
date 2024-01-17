88f4362dea67fc5093b4f5097c6d0c622a4ef8dd
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-01-17 10:11:52 -0800

    Task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array (linear search)
+ * @array: array of integers
+ * @size: size of array
+ * @value: value that should be found
+ *
+ * Return: index of value searched or -1 if it doesn't exist
+ */
+int linear_search(int *array, size_t size, int value)
+{
+size_t a;
+if (array == NULL)
+return (-1);
+for (a = 0; a < size; a++)
+{
+printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+if (value == array[a])
+{
+printf("Value found between indexes [%lu] and [%lu]\n", a, a);
+return (a);
+}
+}
+printf("Value not present in array\n");
+return (-1);
+}
